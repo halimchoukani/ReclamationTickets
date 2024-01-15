895602f81857d62ff87ed924e2c674e886aaa2bf
Rabii <146351459+Rabiikhenissi@users.noreply.github.com>
2024-01-15 14:45:57 +0000

    connexionUpdate1

--- config/connection.php
+++ config/connection.php
@@ -3,7 +3,7 @@ class connection
 {
     public function getConnection()
     {
-        $dbname = "mysql:host=localhost;dbname=reclamation";
+        $dbname = "mysql:host=mysql-zimys2024.alwaysdata.net;dbname=zimys2024_reclamation";
         $user = "zimys2024";
         $pass = "isetrades2024";
         $db = new PDO($dbname, $user, $pass);
