b8cea06938bc2403390cde8bc0b9bea3585207ca
halim choukani <halimchoukani@gmail.com>
2024-01-15 15:08:20 +0100

    Update database name in connection.php

--- config/connection.php
+++ config/connection.php
@@ -3,7 +3,7 @@ class connection
 {
     public function getConnection()
     {
-        $dbname = "mysql:host=localhost;dbname=gestion";
+        $dbname = "mysql:host=localhost;dbname=reclamation";
         $user = "root";
         $pass = "";
         $db = new PDO($dbname, $user, $pass);
