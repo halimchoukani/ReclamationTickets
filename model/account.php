91b3078dd7b8190c43f1cfbbf1d55d1b1dba7d54
halim choukani <halimchoukani@gmail.com>
2024-01-15 14:52:16 +0100

    adding account model

---
+++ model/account.php
@@ -0,0 +1,13 @@
+<?php
+class Compte {
+    private $email;
+    private $nom;
+    private $prenom ;
+    private $tel;
+    private $matricule;
+    private $mot_de_passe;
+    private $type;
+    private $centre;
+    private $status;
+}
+?>
