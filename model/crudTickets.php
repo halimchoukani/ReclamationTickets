2a340f18e87a6d4394e19c40ca2c7a3204aa5528
halim choukani <halimchoukani@gmail.com>
2024-01-15 15:42:16 +0100

    Refactor CRUD operations in crudTickets.php and add auto-increment to ticketId column in reclamation.sql

--- model/crudTickets.php
+++ model/crudTickets.php
@@ -13,38 +13,39 @@ class CrudTicket
     public function CreerTicket($ticket)
     {
         $datenow = date("Y-m-d H:i:s");
-        $req = "INSERT INTO ticket VALUES({$datenow},{$ticket->contact},{$ticket->demande},{},{},{},{},{},{});";
+        $req = "INSERT INTO ticket VALUES({$datenow},{$ticket->getContact()},{$ticket->getDemande()},{$ticket->getDiagnostique()},{$ticket->getCategorie()},{$ticket->getPreorite()},'encour',{null},{null},{null});";
         $stmt = $this->pdo->exec($req);
         return $stmt;
     }
-    public function getAllImmobilier()
+    public function getTickets()
     {
-        $req = "SELECT * FROM immobilier";
+        $req = "SELECT * FROM ticket";
         $stmt = $this->pdo->prepare($req);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_NUM);
     }
-    public function getImmobilier($reference)
+    public function getTicketById($reference)
     {
-        $req = "SELECT * FROM immobilier WHERE reference={$reference}";
+        $req = "SELECT * FROM ticket WHERE reference={$reference}";
         $stmt = $this->pdo->query($req);
         return $stmt->fetch();
     }
-    public function updateImmobilier($immobilier)
+    public function getTicketByCat($cat)
     {
-        $req = "UPDATE immobilier SET proprietaire='{$immobilier->getProprietaire()}',
-        localite='{$immobilier->getLocalite()}',
-        nbpiece={$immobilier->getNbpiece()},
-        domaineUsage='{$immobilier->getDomaineUsage()}',
-        Nature='{$immobilier->getNature()}',
-        nbEtage={$immobilier->getNbEtage()},
-        surface={$immobilier->getSurface()} WHERE reference={$immobilier->getReference()}";
-        $stmt = $this->pdo->exec($req);
-        return $stmt;
+        $req = "SELECT * FROM ticket WHERE reference={$cat}";
+        $stmt = $this->pdo->query($req);
+        return $stmt->fetch();
     }
-    public function deleteImmobilier($reference)
+    public function getByStatus($statut)
     {
-        $req = "DELETE FROM immobilier WHERE reference={$reference}";
+        $req = "SELECT * FROM ticket WHERE statut={$statut}";
+        $stmt = $this->pdo->query($req);
+        return $stmt->fetch();
+    }
+
+    public function supprimerTicket($tickets)
+    {
+        $req = "DELETE FROM immobilier WHERE reference={$tickets}";
         $stmt = $this->pdo->exec($req);
         return $stmt;
     }
