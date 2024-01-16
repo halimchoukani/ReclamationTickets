7944d30702ec12a94b5efb1ea9ceab9fb493a195
rahmabacha <rahmabacha007@gmail.com>
2024-01-16 12:20:17 +0100

    Adding Client Dashboard

---
+++ view/Client/MesTickets.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+</head>
+<body style="background: #F0F0F0;"> 
+<?php include 'NavBarClient.php'; ?>
+    <div style="width: 592px; height: 185px; left: 406px; top: 152px; position: absolute">
+      <div style="width: 592px; height: 82px; left: 0px; top: 0px; position: absolute; text-align: center; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">Mes Tickets</div>
+      <div style="width: 575px; height: 131px; left: 11px; top: 54px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Nous sommes là pour vous aider ! <br/>Pour soumettre un autre ticket<br/><br/>Nous reviendrons vers vous bientôt.</div>
+    </div>
+    <div style="width: 592px; height: 185px; left: 406px; top: 152px; position: absolute">
+      <div style="width: 592px; height: 82px; left: 0px; top: 0px; position: absolute; text-align: center; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">Mes Tickets</div>
+      <div style="width: 575px; height: 131px; left: 11px; top: 54px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Nous sommes là pour vous aider ! <br/>Pour soumettre un autre ticket<br/><br/>Nous reviendrons vers vous bientôt.</div>
+    </div>
+    <div style="width: 1196px; height: 438px; left: 98px; top: 540px; position: absolute">
+      <div style="width: 1196px; height: 0px; left: 0px; top: 42.12px; position: absolute; border: 1px #C0C0C0 solid"></div>
+      <div style="width: 1195.97px; height: 0px; left: 0px; top: 0px; position: absolute; border: 1px #C0C0C0 solid"></div>
+      <div style="width: 161px; height: 23px; left: 39px; top: 8px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Numéro de ticket</div>
+      <div style="width: 46px; height: 23px; left: 365px; top: 8px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Sujet</div>
+      <div style="width: 108px; height: 23px; left: 717px; top: 8px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Description</div>
+      <div style="width: 58px; height: 25px; left: 1072px; top: 7px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Statut</div>
+      <div style="width: 438px; height: 0px; left: 239px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
+      <div style="width: 438px; height: 0px; left: 0px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
+      <div style="width: 438px; height: 0px; left: 536px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
+      <div style="width: 438px; height: 0px; left: 1005px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
+      <div style="width: 438px; height: 0px; left: 1196px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
+    </div>
+    <div style="width: 507px; height: 52.94px; left: 787px; top: 376px; position: absolute; background: #EDEDED; border-radius: 15px"></div>
+    <div style="width: 128px; height: 22px; left: 805px; top: 391.53px; position: absolute; color: #B3B3B3; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Recherche...</div>
+    <div style="width: 37px; height: 37px; left: 1244px; top: 383.53px; position: absolute"></div>
+    <div style="width: 26.96px; height: 26.96px; left: 1248.62px; top: 388.15px; position: absolute;"><i class="fa-solid fa-magnifying-glass"></i></div>
+    <div style="width: 1188px; height: 61px; left: 98px; top: 473px; position: absolute">
+      <div style="width: 48px; height: 35px; left: 939px; top: 1px; position: absolute; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">02</div>
+      <div style="width: 45px; height: 35px; left: 1115px; top: 0px; position: absolute; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">01</div>
+      <img style="width: 28px; height: 28px; left: 1156px; top: 5px; position: absolute" src="../../assets\Time.png" />
+      <img style="width: 26px; height: 26px; left: 980px; top: 9px; position: absolute" src="../../assets\CheckMark.png" />
+      <div style="width: 28px; height: 22px; left: 962px; top: 39px; position: absolute; color: #F79422; font-size: 15px; font-family: Inter; font-weight: 200; word-wrap: break-word">Fait</div>
+      <div style="width: 72px; height: 22px; left: 1115px; top: 39px; position: absolute; color: #F79422; font-size: 15px; font-family: Inter; font-weight: 200; word-wrap: break-word">En attente</div>
+      <div style="width: 342px; height: 18px; left: 0px; top: 0px; position: absolute"><span style="color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">320 Total, </span><span style="color: black; font-size: 20px; font-family: Inter; font-weight: 200; word-wrap: break-word">procéder à leur résolution</span></div>
+      <div style="width: 86px; height: 25px; left: 360px; top: 0px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word ">Période </i></div>
+      <div style="width: 70px; height: 25px; left: 489px; top: 0px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Statut<br/></div> 
+      <div style="width: 1px; height: 0px; left: 565px; top: 4px; position: absolute;"><i class="fas fa-chevron-down"></i></div>
+      <div style="width: 1px; height: 0px; left: 450px; top: 4px; position: absolute;"><i class="fas fa-chevron-down"></i></div>
+    </div>
+
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ view/Client/NavBarClient.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+    <title>Client</title>
+   
+</head>
+<body>
+<nav style="width: 1440px;  padding-top: 27px;  padding-left: 18px; padding-right: 18px; background: #F0F0F0; display: inline-flex ; position: sticky;top: 0;  z-index: 100;">
+  <div style="width: 1404px; height: 90px; position: relative">
+    <div style="width: 1404px; height: 90px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 15px"></div>
+    <img style="width: 140px; height: 64px; left: 27px; top: 13px; position: absolute" src="../../assets/test.png" />
+    <a href="NouveauTicket.php"><div style="width: 187px; height: 26px; left: 426px; top: 31px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; text-transform: uppercase; word-wrap: break-word">nouveau ticket</div></a>
+    <img style="width: 30px; height: 30px; left: 895px; top: 29px; position: absolute" src="../../assets\moncompteicon.png" />
+    <div style="width: 412px; height: 38px; left: 667px; top: 25px; position: absolute">
+    <a href="MesTickets.php"><div style="width: 134px; height: 26px; left: 45px; top: 6px; position: absolute; color: black; font-size: 20px; font-family: Poppins; font-weight: 800; word-wrap: break-word">Mes TICKETS</div></a>
+      <a href="DashboardClient.php"><div style="width: 140px; height: 26px; left: 272px; top: 6px; position: absolute; color: #F79422; font-size: 20px; font-family: Poppins; font-weight: 800; word-wrap: break-word">Mon COMPTE</div></a>
+      <a href="http://"><img style="width: 38px; height: 38px; left: 0px; top: 0px; position: absolute" src="../../assets\ticketicon.png"  /></a>
+      <img style="width: 38px; height: 38px; left: 0px; top: 0px; position: absolute" src="../../assets\ticketicon.png" />
+    </div>
+    <div style="width: 176px; height: 38px; left: 1205px; top: 26px; position: absolute">
+      <div style="width: 138px; height: 38px; left: 38px; top: 0px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Déconnexion</div>
+      <img style="width: 38px; height: 38px; left: 0px; top: 0px; position: absolute" src="../../assets\deconnexionicon.png" />
+    </div>
+  </div>
+</nav>
+</body>
+</html>
