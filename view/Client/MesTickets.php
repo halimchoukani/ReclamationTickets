f2579f3617b2be3786c6138920e1e1900d389e3f
Halim Choukani <halim.choukani@gmail.com>
2024-01-17 10:14:38 -0800

    completer le design d enouveau

--- view/Client/MesTickets.php
+++ view/Client/MesTickets.php
@@ -110,64 +110,19 @@
         <a href=""><img src="../../assets\deconnexionicon.png" /><span>Déconnexion</span></a>
       </li>
     </ul>
-  </nav>
-  <div style="width: 592px; height: 185px; left: 406px; top: 152px; position: absolute">
-    <div style="width: 592px; height: 82px; left: 0px; top: 0px; position: absolute; text-align: center; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">Mes Tickets</div>
-    <div style="width: 575px; height: 131px; left: 11px; top: 54px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Nous sommes là pour vous aider ! <br />Pour soumettre un autre ticket<br /><br />Nous reviendrons vers vous bientôt.</div>
-  </div>
-  <div style="width: 592px; height: 185px; left: 406px; top: 152px; position: absolute">
-    <div style="width: 592px; height: 82px; left: 0px; top: 0px; position: absolute; text-align: center; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">Mes Tickets</div>
-    <div style="width: 575px; height: 131px; left: 11px; top: 54px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Nous sommes là pour vous aider ! <br />Pour soumettre un autre ticket<br /><br />Nous reviendrons vers vous bientôt.</div>
-  </div>
-  <div style="width: 1196px; height: 438px; left: 98px; top: 540px; position: absolute">
-    <div style="width: 1196px; height: 0px; left: 0px; top: 42.12px; position: absolute; border: 1px #C0C0C0 solid"></div>
-    <div style="width: 1195.97px; height: 0px; left: 0px; top: 0px; position: absolute; border: 1px #C0C0C0 solid"></div>
-    <div style="width: 161px; height: 23px; left: 39px; top: 8px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Numéro de ticket</div>
-    <div style="width: 46px; height: 23px; left: 365px; top: 8px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Sujet</div>
-    <div style="width: 108px; height: 23px; left: 717px; top: 8px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Description</div>
-    <div style="width: 58px; height: 25px; left: 1072px; top: 7px; position: absolute; color: black; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Statut</div>
-    <div style="width: 438px; height: 0px; left: 239px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
-    <div style="width: 438px; height: 0px; left: 0px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
-    <div style="width: 438px; height: 0px; left: 536px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
-    <div style="width: 438px; height: 0px; left: 1005px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
-    <div style="width: 438px; height: 0px; left: 1196px; top: 438px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px #C0C0C0 solid"></div>
-  </div>
-  <div style="width: 507px; height: 52.94px; left: 787px; top: 376px; position: absolute; background: #EDEDED; border-radius: 15px"></div>
-
-  <div style="width: 128px; height: 22px; left: 805px; top: 391.53px; position: absolute; color: #B3B3B3; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Recherche...</div>
-
-  <div style="width: 37px; height: 37px; left: 1244px; top: 383.53px; position: absolute"></div>
-  <div style="width: 26.96px; height: 26.96px; left: 1248.62px; top: 388.15px; position: absolute;"><i class="fa-solid fa-magnifying-glass"></i></div>
-  <div style="width: 1188px; height: 61px; left: 98px; top: 473px; position: absolute">
-    <div style="width: 48px; height: 35px; left: 939px; top: 1px; position: absolute; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">02</div>
-    <div style="width: 45px; height: 35px; left: 1115px; top: 0px; position: absolute; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">01</div>
-    <img style="width: 28px; height: 28px; left: 1156px; top: 5px; position: absolute" src="../../assets\Time.png" />
-    <img style="width: 26px; height: 26px; left: 980px; top: 9px; position: absolute" src="../../assets\CheckMark.png" />
-    <div style="width: 28px; height: 22px; left: 962px; top: 39px; position: absolute; color: #F79422; font-size: 15px; font-family: Inter; font-weight: 200; word-wrap: break-word">Fait</div>
-    <div style="width: 72px; height: 22px; left: 1115px; top: 39px; position: absolute; color: #F79422; font-size: 15px; font-family: Inter; font-weight: 200; word-wrap: break-word">En attente</div>
-    <div style="width: 342px; height: 18px; left: 0px; top: 0px; position: absolute"><span style="color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">320 Total, </span><span style="color: black; font-size: 20px; font-family: Inter; font-weight: 200; word-wrap: break-word">procéder à leur résolution</span></div>
-    <div style="width: 86px; height: 25px; left: 360px; top: 0px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word ">Période<i class="fas fa-chevron-down period" style="position: absolute; left: 85px; top:2px">
-        <div class="dropdown-content period-dropdown-content">
-          <a href="#" class="dropdown-option period-dropdown-option">last week</a>
-          <a href="#" class="dropdown-option period-dropdown-option">last month</a>
-          <a href="#" class="dropdown-option period-dropdown-option">last year</a>
-        </div>
-      </i>
-    </div>
-    <div style="width: 70px; height: 25px; left: 489px; top: 0px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Statut<i class="fas fa-chevron-down statut" style="position: absolute; left: 74px; top:2px">
-        <div class="dropdown-content statut-dropdown-content">
-          <a href="#" class="dropdown-option statut-dropdown-option">done</a>
-          <a href="#" class="dropdown-option statut-dropdown-option">in progress</a>
-        </div>
-      </i>
-    </div>
-
-  </div>
+  </nav> 
+  <div class="container">
+    <h2>Mes Tickets</h2>
+    <a class="img-icon" href="../Client/NouveauTicket.php">
+      <img class="icon1" src="../../assets\ticketicon.png" />
+      <img  class="icon2" src="../../assets\plus.png" />
+    </a>
+    <h3>Nous sommes là pour vous aider !</h3>
+    <h3>Pour soumettre un autre ticket </h3><h3> reviendrons vers vous bientôt.</h3>
+    <div class="search-bar"></div>
+    <div class="detail"></div>
   </div>
 
-  </div>
-  </div>
-  </div>
   <script src="../../assets/nav.js
   ">
   </script>
--- view/Client/NouveauTicket.php
+++ view/Client/NouveauTicket.php
@@ -6,10 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="../../assets/style.css">
+  <link rel="stylesheet" href="../../assets/nouveauticket.css">
+  <link rel="stylesheet" href="../../assets/formulaire.css">
 </head>
 
 <body style="background: #F0F0F0;">
-<nav class="nav">
+  <nav class="nav">
     <img class="logo" src="../../assets/test.png" />
     <label class="burger" for="burger">
       <input type="checkbox" id="burger">
@@ -46,30 +48,41 @@
       </li>
     </ul>
   </nav>
-  <div style="width: 1128px; height: 843.42px; left: 138px; top: 164px; position: absolute">
-    <img style="width: 38px; height: 38px; left: 538px; top: 91px; position: absolute" src="../../assets\ticketicon.png" />
-    <div style="width: 592px; height: 82px; left: 268px; top: 0px; position: absolute; text-align: center; color: black; font-size: 35px; font-family: Inter; font-weight: 800; word-wrap: break-word">CREATION DE NOUVEAU <br />TICKET<br /></div>
-    <img style="width: 20px; height: 20px; left: 570px; top: 115px; position: absolute" src="../../assets\plus.png" />
-    <div style="width: 575px; height: 55px; left: 276px; top: 144px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Nous sommes là pour vous aider ! <br />soumettre un ticket ! Nous reviendrons vers vous bientôt.</div>
-    <div style="width: 1128px; height: 0px; left: 0px; top: 244px; position: absolute; border: 1px #C0C0C0 solid"></div>
-    <div style="width: 51px; height: 24px; left: 0px; top: 208px; position: absolute; color: black; font-size: 15px; font-family: Poppins; font-weight: 300; word-wrap: break-word">Details</div>
-    <div style="width: 240px; height: 23px; left: 221px; top: 296px; position: absolute; color: black; font-size: 15px; font-family: Poppins; font-weight: 300; word-wrap: break-word">Choisir la Categorie :</div>
-    <div style="width: 631px; height: 40.54px; left: 232px; top: 343px; position: absolute">
-      <div style="width: 631px; height: 40.54px; left: 0px; top: 0px; position: absolute; background: #F2F2F2; border-radius: 10px; border: 1px black solid"></div>
-      <div style="width: 290px; height: 23px; left: 12px; top: 9px; position: absolute; color: black; font-size: 15px; font-family: Poppins; font-weight: 300; word-wrap: break-word">Les Categories </div>
+  <div class="container">
+    <h2>CREATION DE NOUVEAU TICKET</h2>
+    <div class="img-icon">
+      <img class="icon1" src="../../assets\ticketicon.png" />
+      <img  class="icon2" src="../../assets\plus.png" />
     </div>
-    <div style="width: 1px; height: 0px; left: 830px; top: 357px; position: absolute;"><i class="fas fa-chevron-down"></i></div>
-    <div style="width: 240px; height: 23px; left: 221px; top: 430px; position: absolute; color: black; font-size: 15px; font-family: Poppins; font-weight: 300; word-wrap: break-word">Sujet : </div>
-    <div style="width: 631px; height: 40.54px; left: 232px; top: 477px; position: absolute; background: #F2F2F2; border-radius: 10px; border: 1px black solid"></div>
-    <div style="width: 240px; height: 23px; left: 221px; top: 582px; position: absolute; color: black; font-size: 15px; font-family: Poppins; font-weight: 300; word-wrap: break-word">Description :</div>
-    <div style="width: 631px; height: 105px; left: 232px; top: 629px; position: absolute; background: #F2F2F2; border-radius: 10px; border: 1px black solid"></div>
-    <div style="width: 250.02px; height: 41.42px; left: 409px; top: 802px; position: absolute">
-      <div style="width: 250.02px; height: 41.42px; left: 0px; top: 0px; position: absolute; background: #F79422; border-radius: 15px"></div>
-      <div style="width: 83.34px; height: 22.91px; left: 84.77px; top: 9.25px; position: absolute; color: white; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Envoyer</div>
+    <h3>Nous sommes là pour vous aider !</h3>
+    <h3>soumettre un ticket ! Nous reviendrons vers vous bientôt.</h3>
+    <div class="title">
+      <h4>Details</h4>
+      <div class="bar"></div>
     </div>
+    <form action="" class="form">
+      <div class="div">
+        <label for="">1. Choisir la Categorie : </label>
+        <select class="input" name="" id="">
+          <option value="">Choisir la Categorie</option>
+          <option value="">Choisir la Categorie</option>
+          <option value="">Choisir la Categorie</option>
+          <option value="">Choisir la Categorie</option>
+        </select>
+      </div>
+      <div class="div">
+        <label for="">2. Sujet : </label>
+        <input type="text" class="input">
+      </div>
+      <div class="div">
+        <label for="">3. Description : </label>
+        <textarea name="" class="input" id="" cols="30" rows="10"></textarea>
+      </div>
+      <button type="submit">Envoyer</button>
+    </form>
   </div>
-  </div>
-  </div>
+  <script src="../../assets/nav.js
+  "></script>
 </body>
 
 </html>
