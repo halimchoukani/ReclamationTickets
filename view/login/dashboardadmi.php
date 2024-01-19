c2caca3e08293daed2bdea8395a317deaf2c4793
manel hichri <manelhichri30@gmail.com>
2024-01-19 16:02:08 +0100

    manel

---
+++ view/login/dashboardadmi.php
@@ -0,0 +1,121 @@
+<html style="width: 1200px; height: 1000px; padding-top: 29px; padding-bottom: 30px; padding-left: 18px; padding-right: 18px; background: #F0F0F0; background-size: cover">
+        <div style="width: 500px; height: 500px; position: relative">
+          <div style="width: 1404px; height: 90px; left: 0px; top: 0px; position: absolute">
+            <div style="width: 1404px; height: 90px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 15px"></div>
+            <img style="width: 140px; height: 64px; left: 27px; top: 13px; position: absolute" src="logo.png" />
+            <div style="width: 130px; height: 30px; left: 577px; top: 26px; position: absolute">
+              
+              <img style="width: 38px; height: 38px; left: 0px; top: 0px; position: absolute" src="image 2 (1).png" />
+              <a href=""style="width: 91px; height: 26px; left: 48px; top: 6px; position: absolute; color: #F29427; font-size: 20px; font-family: Poppins; font-weight: 800; text-transform: uppercase; word-wrap: break-word">aperçu</a>
+            </div>
+            <div style="width: 139px; height: 38px; left: 768px; top: 26px; position: absolute">
+              <a href=""style="width: 104px; height: 38px; left: 35px; top: 6px; position: absolute; color: black; font-size: 20px; font-family: Poppins; font-weight: 800; text-transform: uppercase; word-wrap: break-word">comptes</a>
+              <img style="width: 30px; height: 30px; left: 0px; top: 4px; position: absolute" src="image 3.png" />
+            </div>
+            <div style="width: 134px; height: 38px; left: 959px; top: 26px; position: absolute">
+              <a href="" style="width: 89px; height: 26px; left: 45px; top: 6px; position: absolute; color: black; font-size: 20px; font-family: Poppins; font-weight: 800; word-wrap: break-word">TICKETS</a>
+              
+              <img style="width: 38px; height: 38px; left: 0px; top: 0px; position: absolute" src="image 4.png" />
+            </div>
+            <div style="width: 176px; height: 38px; left: 1205px; top: 26px; position: absolute">
+              <a href="" style="width: 138px; height: 38px; left: 38px; top: 6px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 800; word-wrap: break-word">Déconnexion</a>
+              <img style="width: 38px; height: 38px; left: 0px; top: 0px; position: absolute" src="image 6.png" />
+            </div>
+          </div>
+      
+          <div style="width: 1404px; height: 592px; left: 0px; top: 120px; position: absolute">
+            <div style="width: 1404px; height: 592px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 15px"></div>
+            <img style="width: 45px; height: 38px; left: 34px; top: 33px; position: absolute" src="image 4.png" />
+            <div style="width: 507px; height: 52.94px; left: 874px; top: 26.47px; position: absolute">
+              <input type="text" placeholder="Recherche..."  style="width: 507px; height: 52.94px; left: 0px; top: 0px; position: absolute; background: #EDEDED; border-radius: 15px">
+              <div style="width: 37px; height: 37px; left: 457px; top: 7.53px; position: absolute">
+                <div style="width: 37px; height: 37px; left: 0px; top: 0px; position: absolute"></div>
+                <img style="width: 45px; height: 38px; left: 0px; top: 1px; position: absolute" src="Vector.png" />
+              </div>
+            </div>
+          </div>
+          <div style="width: 680px; height: 396px; left: 0px; top: 742px; position: absolute">
+            <div style="width: 680px; height: 396px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 15px"></div>
+            <div style="width: 26px; height: 30.39px; left: 13px; top: 11px; position: absolute">
+              <div style="width: 19.50px; height: 22.79px; left: 3.25px; top: 3.79px; position: absolute; background: #2A343D"></div>
+            </div>
+            <div style="width: 262px; height: 26px; left: 39px; top: 13px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Incrustation de graphique</div>
+          </div>
+          <div style="width: 405px; height: 42px; left: 88px; top: 153px; position: absolute; text-align: center; color: black; font-size: 35px; font-family: Poppins; font-weight: 800; word-wrap: break-word">Derniers Reclamation<br/></div>
+          <div style="width: 44px; height: 35px; left: 1091px; top: 220px; position: absolute; color: black; font-size: 35px; font-family: Poppins; font-weight: 800; word-wrap: break-word">20</div>
+          <div style="width: 38px; height: 35px; left: 1268px; top: 220px; position: absolute; color: black; font-size: 35px; font-family: Poppins; font-weight: 800; word-wrap: break-word">12</div>
+          <img style="width: 28px; height: 28px; left: 1309px; top: 224px; position: absolute" src="image 34.png" />
+          <img style="width: 26px; height: 26px; left: 1141px; top: 225px; position: absolute" src="image 13.png" />
+          <div style="width: 68px; height: 22px; left: 1107px; top: 265px; position: absolute; color: #F79422; font-size: 15px; font-family: Inter; font-weight: 200; word-wrap: break-word">Fait<br/></div>
+          <div style="width: 86px; height: 22px; left: 1261px; top: 265px; position: absolute; color: #F79422; font-size: 15px; font-family: Inter; font-weight: 200; word-wrap: break-word">En attente</div>
+          <div style="width: 350px; height: 30px; left: 50px; top: 242px; position: absolute"><span style="color: black; font-size: 20px; font-family: Poppins; font-weight: 800; word-wrap: break-word">Total </span><span style="color: black; font-size: 20px; font-family: Poppins; font-weight: 250; word-wrap: break-word">procéder à leur résolution</span></div>
+          <div style="width: 1328px; height: 0px; left: 38px; top: 704px; position: absolute; border: 1px #C0C0C0 solid"></div>
+          <div style="width: 70px; height: 25px; left: 422px; top: 239px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 800; word-wrap: break-word">Statut<br/></div>
+          <select style="width: 40px; height: 40px; left: 510px; top: 256px; position: absolute;">
+            <option>En attente</option>
+            <option>Resolu</option>
+          </select>
+          <div style="width: 680px; height: 396px; left: 724px; top: 742px; position: absolute; background: #D9D9D9; border-radius: 15px"></div>
+          <div style="width: 209px; height: 26px; left: 768px; top: 755px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Top Administrateurs</div>
+          <div style="width: 26px; height: 26px; left: 742px; top: 755px; position: absolute">
+            <div style="width: 8.67px; height: 8.67px; left: 8.67px; top: 4.33px; position: absolute; border-radius: 9999px; border: 2px #33363F solid"></div>
+            <div style="width: 6.50px; height: 6.50px; left: 15.17px; top: 6.50px; position: absolute; border-radius: 9999px; border: 2px #33363F solid"></div>
+            <div style="width: 6.50px; height: 6.50px; left: 4.33px; top: 6.50px; position: absolute; border-radius: 9999px; border: 2px #33363F solid"></div>
+            <img style="width: 6.51px; height: 4.33px; left: 16.01px; top: 15.17px; position: absolute; border: 2px #33363F solid" src="" />
+            <img style="width: 6.51px; height: 4.33px; left: 3.48px; top: 15.17px; position: absolute; border: 2px #33363F solid" src="" />
+            <div style="width: 10.83px; height: 5.42px; left: 7.58px; top: 15.17px; position: absolute; border: 2px #33363F solid"></div>
+          </div>
+          <img style="width: 35px; height: 35px; left: 850px; top: 853px; position: absolute" src="" />
+          <div style="width: 434px; height: 45px; left: 842px; top: 805px; position: absolute; background: #F79422; border-radius: 15px"></div>
+          <div style="width: 448px; height: 0px; left: 840px; top: 802px; position: absolute; border: 1px black solid"></div>
+          <div style="width: 448px; height: 0px; left: 840px; top: 1028px; position: absolute; border: 1px black solid"></div>
+          <div style="width: 295px; height: 35px; left: 981px; top: 810px; position: absolute">
+            <div style="width: 69px; height: 35px; left: 0px; top: 0px; position: absolute; color: #474747; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 65px; height: 35px; left: 126px; top: 0px; position: absolute; color: #474747; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 47px; height: 35px; left: 248px; top: 0px; position: absolute; color: #474747; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+          </div>
+          <div style="width: 295px; height: 35px; left: 981px; top: 853px; position: absolute">
+            <div style="width: 69px; height: 35px; left: 0px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 65px; height: 35px; left: 126px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 47px; height: 35px; left: 248px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+          </div>
+          <div style="width: 295px; height: 35px; left: 981px; top: 896px; position: absolute">
+            <div style="width: 69px; height: 35px; left: 0px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 65px; height: 35px; left: 126px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 47px; height: 35px; left: 248px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+          </div>
+          <div style="width: 295px; height: 35px; left: 981px; top: 939px; position: absolute">
+            <div style="width: 69px; height: 35px; left: 0px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 65px; height: 35px; left: 126px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 47px; height: 35px; left: 248px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+          </div>
+          <div style="width: 295px; height: 35px; left: 981px; top: 982px; position: absolute">
+            <div style="width: 69px; height: 35px; left: 0px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 65px; height: 35px; left: 126px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+            <div style="width: 47px; height: 35px; left: 248px; top: 0px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+          </div>
+          <div style="width: 249px; height: 40px; left: 768px; top: 1062px; position: absolute; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Voter Classement</div>
+          <div style="width: 35px; height: 35px; left: 849px; top: 939px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">4</div>
+          <div style="width: 35px; height: 35px; left: 849px; top: 982px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">5</div>
+          <img style="width: 35px; height: 35px; left: 852px; top: 810px; position: absolute" src="" />
+          <img style="width: 35px; height: 35px; left: 852px; top: 896px; position: absolute" src="" />
+          <div style="width: 1352px; height: 408px; left: 16px; top: 295px; position: absolute">
+
+            <table border="1">
+              <tr>
+             <td>Demande</td>
+            <td>Diagnostic</td>
+            <td >Contact</td>
+            <td >Clôturé par</td>
+            <td >Client</td>
+            <td>Priorité</td>
+            <td>Statut</td>
+            <td >Catégorie</td>
+            <td  >Date & Heure</td>
+            <td  >Date & Heure de Clôture</td>
+           </tr>
+           </table> 
+          </div>
+        </div>
+      </html>
+      
---
+++ view/login/step2.php
@@ -0,0 +1,97 @@
+    
+<html style="width: 1200px; height: 1000px; position: relative; background:#F0EBE5;background-size: cover; backdrop-filter: blur(100px)">
+    <div style="flex: 1 1 0; align-self: stretch; padding-top: 50px; padding-bottom: 50px; padding-left: 200px; padding-right: 320px; background: linear-gradient(180deg, #D9D9D9 0%, rgba(255, 255, 255, 0) 100%); border: 1px #D8D8D8 solid; justify-content: center; align-items: center; display: inline-flex;border-radius: 15px;">
+      <div style="width: 631px; height: 641px; position: relative">
+<center>
+<img style="width: 220px; height: 91px" src="logo.png" />
+</center>
+<br>
+<br>
+<div style="width: 943px; height: 67px; position: relative">
+  <div style="width: 163px; height: 67px; left: 0px; top: 0px; position: absolute">
+    <div style="width: 163px; height: 67px; left: 0px; top: 0px; position: absolute; background: #F79422; border-radius: 15px"></div>
+    <div style="width: 163px; height: 67px; left: 0px; top: 18px; position: absolute; text-align: center; color: white; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Etape 1</div>
+  </div>
+  <div style="width: 163px; height: 67px; left: 260px; top: 0px; position: absolute">
+    <div style="width: 163px; height: 67px; left: 0px; top: 0px; position: absolute; background: #F79422; border-radius: 15px"></div>
+    <div style="width: 163px; height: 67px; left: 0px; top: 18px; position: absolute; text-align: center; color: white; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Etape 2</div>
+  </div>
+  <div style="width: 163px; height: 67px; left: 520px; top: 0px; position: absolute">
+    <div style="width: 163px; height: 67px; left: 0px; top: 0px; position: absolute; background: #FABA71; border-radius: 15px"></div>
+    <div style="width: 163px; height: 67px; left: 0px; top: 18px; position: absolute; text-align: center; color: #494949; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Etape 3</div>
+    <div style="width: 163px; height: 67px; left: 0px; top: 18px; position: absolute; text-align: center; color: #727070; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Etape 3</div>
+  </div>
+  <div style="width: 163px; height: 67px; left: 780px; top: 0px; position: absolute">
+    <div style="width: 163px; height: 67px; left: 0px; top: 0px; position: absolute; background: #FABA71; border-radius: 15px"></div>
+    <div style="width: 163px; height: 67px; left: 0px; top: 18px; position: absolute; text-align: center; color: #727070; font-size: 20px; font-family: Inter; font-weight: 800; word-wrap: break-word">Etape 4</div>
+  </div>
+
+
+  
+<img style="left: 171px; top: 33px; position: absolute;"  src="flech.png" >
+<img style="left: 431px; top: 33px; position: absolute;" src="flech.png" >
+<img style="left: 691px; top: 33px; position: absolute;" src="flech.png" >
+</div>
+<br>
+<center>
+<div style="width: 84px; height: 32px; text-align: center; color: black; font-size: 24px; font-family: Inter; font-weight: 800; word-wrap: break-word">Type</div>
+<input type="checkbox" name="" style="font-size: 50px;  width: 21px; height: 21px; background: #676767; border-radius: 20px; border: 0.50px black solid;cursor: pointer"><b>Individuel</b>
+<input type="checkbox" name="" style="width: 21px; height: 21px; background: #676767; border-radius: 20px; border: 0.50px black solid"><b>Buisness</b>
+
+
+</center>
+<table>
+    <tr>
+      <td>
+        <div style="width: 88px; height: 30px; color: black; font-size: 24px; font-family: Inter; font-weight: 800; word-wrap: break-word">Nom</div>
+      </td>
+      <td>
+        <div style="width: 125px; height: 30px; color: black; font-size: 24px; font-family: Inter; font-weight: 800; word-wrap: break-word">Prenom</div>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input type="text" style="width: 316px; height: 40px; background: #EEEEEE; border-radius: 10px; border: 1px black solid;left: 300px;">
+      </td>
+      <td>
+        <input type="text"style="width: 316px; height: 40px; background: #EEEEEE; border-radius: 10px; border: 1px black solid">
+
+      </td>
+    </tr>
+  </table>
+  <br>
+  <br>
+  <div style="width: 162.52px; height: 60.67px; color: black; font-size: 24px; font-family: Inter; font-weight: 800; word-wrap: break-word">Genre</div>
+  <table>
+    <tr>
+        <td>
+  <input type="checkbox"style="width: 30.10px; height: 30.33px; background: #D9D9D9; border-radius: 15px; border: 0.50px black solid">
+        </td>
+        <td>
+  <div style="width: 98.31px; height: 30.33px; color: black; font-size: 20px; font-family: Inter; font-weight: 300; word-wrap: break-word">Femme</div>
+</td>
+<td>
+    <input type="checkbox"style="width: 30.10px; height: 30.33px; background: #D9D9D9; border-radius: 15px; border: 0.50px black solid">
+          </td>
+          <td>
+    <div style="width: 98.31px; height: 30.33px; color: black; font-size: 20px; font-family: Inter; font-weight: 300; word-wrap: break-word">Homme</div>
+  </td>
+  </tr>
+  
+</table>
+
+<div style="width: 114.34px; height: 47px; left: 650px; top: 600px; position: absolute">
+  <input type="button" value="Annuler" name="" style="width: 114.34px; height: 47px; left: 0px; top: 0px; position: absolute; background: #F72222; border-radius: 15px ;right: 1000px; color: white; font-size: 20px">
+  <div style="width: 114.34px; height: 47px; left: 0px; top: 0px; position: absolute; text-align: center; color: #F3F3F3; font-size: 16px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+</div>
+
+<div style="width: 114.34px; height: 47px; left: 800px; top: 600px; position: absolute">
+  <input type="button" value="Suivant" name="" style="width: 114.34px; height: 47px; left: 0px; top: 0px; position: absolute; background: #F79422; border-radius: 15px ;right: 1000px; color: white; font-size: 20px">
+  <div style="width: 114.34px; height: 47px; left: 0px; top: 0px; position: absolute; text-align: center; color: #F3F3F3; font-size: 16px; font-family: Inter; font-weight: 800; word-wrap: break-word"></div>
+</div>
+
+
+
+  </div>
+  </div>
+</html>
