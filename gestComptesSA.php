b178a6d75875d9fe9b16cdbc881fd2204cb78e31
Rabii khenissi <rabiikhenissi124@gmail.com>
2024-01-22 14:32:33 +0100

    fixgestCompt

--- gestComptesSA.php
+++ gestComptesSA.php
@@ -128,15 +128,25 @@ session_start();
 				<ul class="metismenu" id="menu">
 					<li><a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
 							<i class="flaticon-381-networking"></i>
-							<span class="nav-text">Apercu</span>
+							<span class="nav-text">Dashboard</span>
 						</a>
-
+						<ul aria-expanded="false">
+							<li><a href="index.html">Dashboard</a></li>
+							<li><a href="analytics.html">Analytics</a></li>
+						</ul>
 					</li>
 					<li>
 						<a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
 							<i class="flaticon-381-diploma"></i>
 							<span class="nav-text">Comptes </span>
 						</a>
+						<ul aria-expanded="false">
+							<li><a href="index.html">Gestion Des Comptes</a></li>
+							<?php if ($_SESSION['type']=="admin"){} 
+							echo "<li><a href='analytics.html'>Ajouter Un Compte</a></li>";
+							?>
+						</ul>
+						
 
 					</li>
 					<li><a class="has-arrow ai-icon" href="javascript:void(0)" aria-expanded="false">
@@ -160,93 +170,17 @@ session_start();
             EventList
         ***********************************-->
 
-		<div class="event-sidebar dz-scroll" id="eventSidebar">
-			<div class="card shadow-none rounded-0 bg-transparent h-auto mb-0">
-				<div class="card-body text-center event-calender pb-2">
-					<input type='text' class="form-control d-none" id='datetimepicker1'>
-				</div>
-			</div>
-			<div class="card shadow-none rounded-0 bg-transparent h-auto">
-				<div class="card-header border-0 pb-0">
-					<h4 class="text-black">Upcoming Events</h4>
-				</div>
-				<div class="card-body">
-					<div class="media mb-5 align-items-center event-list">
-						<div class="p-3 text-center rounded me-3 date-bx bgl-primary">
-							<h2 class="mb-0 text-black">3</h2>
-							<h5 class="mb-1 text-black">Wed</h5>
-						</div>
-						<div class="media-body px-0">
-							<h6 class="mt-0 mb-3 fs-14"><a class="text-black" href="events.html">Live Concert Choir Charity Event 2020</a></h6>
-							<ul class="fs-14 list-inline mb-2 d-flex justify-content-between">
-								<li>Ticket Sold</li>
-								<li>561/650</li>
-							</ul>
-							<div class="progress mb-0" style="height:4px; width:100%;">
-								<div class="progress-bar bg-warning progress-animated" style="width:85%; height:100%;" role="progressbar">
-									<span class="sr-only">60% Complete</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="media mb-5 align-items-center event-list">
-						<div class="p-3 text-center rounded me-3 date-bx bgl-primary">
-							<h2 class="mb-0 text-black">16</h2>
-							<h5 class="mb-1 text-black">Tue</h5>
-						</div>
-						<div class="media-body px-0">
-							<h6 class="mt-0 mb-3 fs-14"><a class="text-black" href="events.html">Beautiful Fireworks Show In The New Year Night</a></h6>
-							<ul class="fs-14 list-inline mb-2 d-flex justify-content-between">
-								<li>Ticket Sold</li>
-								<li>431/650</li>
-							</ul>
-							<div class="progress mb-0" style="height:4px; width:100%;">
-								<div class="progress-bar bg-warning progress-animated" style="width:50%; height:100%;" role="progressbar">
-									<span class="sr-only">60% Complete</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="media mb-0 align-items-center event-list">
-						<div class="p-3 text-center rounded me-3 date-bx bgl-success">
-							<h2 class="mb-0 text-black">28</h2>
-							<h5 class="mb-1 text-black">Fri</h5>
-						</div>
-						<div class="media-body px-0">
-							<h6 class="mt-0 mb-3 fs-14"><a class="text-black" href="events.html">The Story Of Danau Toba (Musical Drama)</a></h6>
-							<ul class="fs-14 list-inline mb-2 d-flex justify-content-between">
-								<li>Ticket Sold</li>
-								<li>650/650</li>
-							</ul>
-							<div class="progress mb-0" style="height:4px; width:100%;">
-								<div class="progress-bar bg-success progress-animated" style="width:100%; height:100%;" role="progressbar">
-									<span class="sr-only">60% Complete</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="card-footer justify-content-between border-0 d-flex fs-14">
-					<span>5 events more</span>
-					<a href="events.html" class="text-primary">View more <i class="las la-long-arrow-alt-right scale5 ms-2"></i></a>
-				</div>
-			</div>
-			<div class="card shadow-none rounded-0 bg-transparent h-auto mb-0">
-				<div class="card-body text-center event-calender">
-					<a href="javascript:void(0);" class="btn btn-primary btn-rounded btn shadow" data-bs-toggle="modal" data-bs-target="#exampleModal">
-						+ New Event
-					</a>
-				</div>
-			</div>
-		</div>
+	
 		<div class="content-body">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="card">
 							<div class="card-header flex-wrap">
-								<h4 class="card-title">LES COMPTES</h4>
-								<a href="add-customers.html" class="btn btn-primary me-3 mt-2 mt-sm-0"><i class="feather feather-user-plus"></i> Add Customers</a>
+								<h4 class="card-title">LES COMPTES</h4><?php if ($_SESSION['type']=="admin"){} 
+							echo "<a href='add-customers.html' class='btn btn-primary me-3 mt-2 mt-sm-0'><i class='feather feather-user-plus'></i> Ajouter Compte</a>";
+							?>
+								
 							</div>
 							<div class="card-body">
 								<div class="table-responsive ticket-table ">
@@ -419,7 +353,7 @@ session_start();
 	<script src="assets/js/custom.min.js"></script>
 	<script src="assets/js/deznav-init.js"></script>
 	<script src="assets/js/demo.js"></script>
-	<script src="asses/js/styleSwitcher.js"></script>
+	<script src="assets/js/styleSwitcher.js"></script>
 
 </body>
 
