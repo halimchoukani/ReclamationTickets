cd4a177e7833ee2713a29b191f7d8493c79c9824
Aziz Ben Salem <bensalemaziz58168@gmail.com>
2024-01-23 11:19:27 +0100

    tre

--- controller/ticket/cloture.php
+++ controller/ticket/cloture.php
@@ -1,14 +1,10 @@
 <?php
 require_once("../../CRUD/crudtickets.php");
 $crud=new CrudTicket();
-if (isset($_POST['diag'])) {
-    
+if (isset($_POST['diag'])) { 
 }else{
     if(isset($_GET['id'])){
         echo $crud->getDiagByid($_GET['id']);
     }
-
 }
-
-
 ?>
